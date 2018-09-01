c0d9b61daf83ff9986f500dd86ca336dc434128a
Rohan Kapoor <rohan@rohankapoor.com>
2018-09-01 00:55:28 -0700

    Python chagnes to resetting all HASP displays due to HA sandbox

--- python_scripts/hasp_reset_all_screens.py
+++ python_scripts/hasp_reset_all_screens.py
@@ -12,29 +12,33 @@ def get_nodename(entity_id):
     if not entity_id.startswith('input_number.'):
         return None
 
-    start = entity_id.find('input_number.') + 13
-    end = entity_id.find('_activepage')
+    prefix = 'input_number.hasp_'
+    suffix = '_activepage'
+    start = entity_id.find(prefix)
+    end = entity_id.find(suffix)
     if start == -1 or end == -1:
         return None
-    return entity_id[start:end]
+    return entity_id[(start + len(prefix)):end]
 
-def reset_hasp_screen(entity_id):
+def reset_hasp_screen(hass, entity_id):
+    global get_nodename
     nodename = get_nodename(entity_id)
     if not nodename:
         return
 
-    cur_state = hass.state.get(entity_id)
+    cur_state = hass.states.get(entity_id)
     if not cur_state:
         return
 
     hass.services.call('mqtt', 'publish', {
         'topic': 'hasp/{}/command/page'.format(nodename),
-        'payload': cur_state.state
+        'payload': int(float(cur_state.state))
     })
+    return
 
 entities = data.get('entities')
 
 if isinstance(entities, str):
     entities = [e.strip() for e in entities.split(',')]
     for entity_id in entities:
-        reset_hasp_screen(entity_id)
+        reset_hasp_screen(hass, entity_id)
