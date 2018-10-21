a6d9b69f0f4faf1133943a06de0f98a09a05c185
Rohan Kapoor <rohan@rohankapoor.com>
2018-10-21 11:18:46 -0700

    Fixing a bug with counting speakers_playing

--- python_scripts/media_music_turn_off_speakers.py
+++ python_scripts/media_music_turn_off_speakers.py
@@ -7,12 +7,14 @@
 #   entities: A list of remote entities that should be turned off
 ###############################################################################
 
+
 def decrement_speakers_playing(hass):
     entity_id = 'input_number.speakers_playing'
     speakers_playing = hass.states.get(entity_id)
-    if not (speakers_playing
-            and speakers_playing.attributes
-            and speakers_playing.attributes.get('min') is not None
+    if not (
+        speakers_playing
+        and speakers_playing.attributes
+        and speakers_playing.attributes.get('min') is not None
     ):
         return
     count = int(float(speakers_playing.state))
@@ -24,6 +26,7 @@ def decrement_speakers_playing(hass):
                            }
                        )
 
+
 def turn_off_speaker(hass, entity_id):
     cur_state = hass.states.get(entity_id)
     if not (cur_state
@@ -32,14 +35,16 @@ def turn_off_speaker(hass, entity_id):
             and cur_state.attributes.get('music_activity_name') is not None
             and cur_state.attributes.get('music_activity_name') == cur_state.attributes.get('current_activity')
     ):
-        return
+        return False
 
     hass.services.call('remote', 'turn_off', {'entity_id': entity_id})
+    return True
+
 
 entities = data.get('entities')
 
 if isinstance(entities, str):
     entities = [e.strip() for e in entities.split(',')]
-    for entity_id in entities:
-        decrement_speakers_playing(hass)
-        turn_off_speaker(hass, entity_id)
+    for entity in entities:
+        if turn_off_speaker(hass, entity):
+            decrement_speakers_playing(hass)
