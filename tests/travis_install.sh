096afb0c238eba5cf8e329f79b4004a2ad388c54
Rohan Kapoor <rohan@rohankapoor.com>
2020-11-26 15:44:50 -0800

    Fix up copy paste

--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -2,7 +2,7 @@
 
 version=`cat .HA_VERSION`
 if [ "$version" = "" ]; then
-    python3 -m pip install --disable-pip-version-check homeassistant install homeassistant
+    python3 -m pip install --disable-pip-version-check homeassistant
 else
-    python3 -m pip install --disable-pip-version-check homeassistant install homeassistant==${version}
+    python3 -m pip install --disable-pip-version-check homeassistant==${version}
 fi
