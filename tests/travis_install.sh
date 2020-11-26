209edd1d6360ecf99b99da99d8cb3d08e15bc7da
Rohan Kapoor <rohan@rohankapoor.com>
2020-11-26 15:38:40 -0800

    Reformat

--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -2,7 +2,7 @@
 
 version=`cat .HA_VERSION`
 if [ "$version" = "" ]; then
-    pip3 install homeassistant
+    python3 -m pip install --disable-pip-version-check homeassistant install homeassistant
 else
-    pip3 install homeassistant==${version}
+    python3 -m pip install --disable-pip-version-check homeassistant install homeassistant==${version}
 fi
