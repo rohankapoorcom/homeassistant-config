1ea07f672d40e1f5c5c0e1ec4936643d517da3e3
Rohan Kapoor <rohan@rohankapoor.com>
2024-10-19 18:04:04 -0700

    Setup Dymo 450 Duo label printer in kitchen

---
+++ shell_command/print_label.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh -o StrictHostKeyChecking=accept-new -i /srv/homeassistant/.ssh/id_ed25519 lprint@001-lprint-01.dev.rohankapoor.com "convert -size 673x378 -background white -fill black -font 'AvantGarde-Book' -gravity Center label:'$1' label.png;/snap/bin/lprint -d 'DYMO LabelWriter 450 DUO Label' label.png -n '$2'"
